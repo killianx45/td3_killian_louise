ecfbd1bf1278071eef0d34f23ab1a2a1bd7589ad
Killian <killianjeuffroy@gmail.com>
2025-12-01 14:01:59 +0100

    script pour automatisé sur alwaysdata

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Aller dans ton dossier web
+cd /home/killian/www/td3_killian_louise
+
+# Mettre à jour depuis GitHub
+git pull origin main
+
+# Donner les permissions correctes si nécessaire
+chmod -R 755 /home/killian/www/td3_killian_louise
